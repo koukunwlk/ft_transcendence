8b4b4787705839b6dc936af3b431f7ecadfecf47
Atila Costa Almeida <chaves_87@hotmail.com>
2023-11-22 20:19:30 -0300

    refactor/login_page

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,9 +1,33 @@
+<template>
+  <div class="main">
+    <h1>Pong Login Page!</h1>
+    <input
+      type="text"
+      name="username"
+      id="username"
+      placeholder="UserName"
+    />
+    <br />
+    <input
+      type="password"
+      name="password"
+      id="password"
+      placeholder="password que nao vai precisar"
+    />
+    <br />
+    <input
+      type="button"
+      value="Done!"
+      class="button"
+      id="done"
+    />
+    <br />
+    <img alt="Login as Guest" />
+  </div>
+</template>
+
 <script>
 export default {
-	name: "Login",
+  name: "Login",
 };
 </script>
-
-<template>
-	<h1>Login Page</h1>
-</template>
