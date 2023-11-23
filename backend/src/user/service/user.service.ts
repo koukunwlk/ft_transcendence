f5ef265348dc6864ca2d40dfcb69d73188a579b9
gustavo <gustavdlima@gmail.com>
2023-11-23 20:52:43 -0300

    feat: add insertUser function

--- backend/src/user/service/user.service.ts
+++ backend/src/user/service/user.service.ts
@@ -1,6 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { UserRepository } from '../repository/user.repository';
+import { User } from '../domain/model/user/user';
 
 @Injectable()
 export class UserService {
-  
+  constructor(
+	@Inject(UserRepository)
+	private readonly userRepository: UserRepository
+  ) { }
+
+  async insertUser(nickname: string) :Promise<string>{
+	const user = new User({
+		nickname
+	})
+	const id = await this.userRepository.insert(user)
+
+	return id
+  }
 }
