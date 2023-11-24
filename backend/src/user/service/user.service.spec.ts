1b4041da0e8fa527a731892819c93077ec44b7e1
Atila Costa Almeida <chaves_87@hotmail.com>
2023-11-23 21:53:31 -0300

    feat:add duplicated nickname validation

--- backend/src/user/service/user.service.spec.ts
+++ backend/src/user/service/user.service.spec.ts
@@ -11,14 +11,25 @@ describe('UserService', () => {
   it("Should be defined", () => {
     expect(userService).toBeDefined()
   })
-  it("Should insert a new user", () => {
+  it("Should insert a new user", async () => {
     // arrange
     let nickname: string = "ArnaldoCoelho"
 
     // act
-    userService.insertUser(nickname)
+    await userService.insertUser(nickname)
 
     // assert
     expect(inMemoryDb.getSize()).toEqual(1)
+
+  })
+  it("Do not insert duplicated user", async () => {
+    //arrange
+    let nickname: string = "Test"
+
+    //act
+    await userService.insertUser(nickname)
+
+    //assert
+    expect(userService.insertUser(nickname)).rejects.toThrow(`nickname: ${nickname} already exists`)    
   })
 });
--- backend/src/user/service/user.service.ts
+++ backend/src/user/service/user.service.ts
@@ -1,4 +1,4 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { BadRequestException, Inject, Injectable } from '@nestjs/common';
 import { UserRepository } from '../repository/user.repository';
 import { User } from '../domain/model/user/user';
 
@@ -10,11 +10,19 @@ export class UserService {
   ) { }
 
   async insertUser(nickname: string) :Promise<string>{
+	await this.checkDuplicatedUser(nickname);
 	const user = new User({
 		nickname
 	})
 	const id = await this.userRepository.insert(user)
 
 	return id
+  }
+  private async checkDuplicatedUser(nickname: string) {
+	const user = await this.userRepository.findOne({nickname})
+	
+	if (user){
+		throw new BadRequestException(`nickname: ${nickname} already exists`)
+	}
   }
 }
