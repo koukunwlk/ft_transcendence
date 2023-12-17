9c9b05dc908bbca37e714d11bce0fc63768814b8
willian <willianportilho01@gmail.com>
2023-12-17 20:23:16 -0300

    feat: added initial Login page implementation

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -5,5 +5,44 @@ export default {
 </script>
 
 <template>
-	<h1>Login Page</h1>
+	<div class="main">
+		<img class="background" src="../Assets/Images/viktor-forgacs.jpg" alt="background">
+		<div class="login-box">
+		</div>
+	</div>
 </template>
+
+<style>
+
+body {
+	margin: 0;
+	overflow: hidden;
+}
+
+.main {
+	width: 100vw;
+	height: 100vh;
+}
+
+.background {
+	width: 100vw;
+	height: 100vh;
+	justify-content: center;
+	align-items: center;
+    object-fit: cover;
+}
+
+.login-box {
+	position: absolute;
+	top: 50%;
+	left: 50%;
+	transform: translate(-50%, -50%);
+	height: 500px;
+	width: 350px;
+	background-color: rgba(255, 255, 255, 0.05);
+	backdrop-filter: blur(6px);
+	border-radius: 24px;
+	border: 1px solid rgba(255, 255, 255, 0.2);
+}
+
+</style>
