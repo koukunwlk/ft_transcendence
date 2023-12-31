8fceda4b50c4eb813cc8bf4be795efe1428abe89
gustavdlima <gustavdlima@gmail.com>
2023-12-30 21:03:13 -0300

    feat: add avatar component on Navbar.vue

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -1,11 +1,18 @@
 <template>
-	<div class="flex justify-end">
-		<div class="text-4xl text-zinc-500 ">
-		  Profile
+	<div class="flex h-full justify-end">
+		<div class="sm:h-20 sm:w-28 grid justify-center">
+			<Avatar name="G" size="80" status="online" borderColor="black" ></Avatar>
 		</div>
 	</div>
 </template>
 
 <script>
+import Avatar from "vue3-avatar"
 
+export default {
+	name: "Navbar",
+	components: {
+		Avatar: Avatar,
+	},
+};
 </script>
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,8 +2,10 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './Router/index';
 import './styles/style.css';
+import Avatar from "vue3-avatar";
 
 createApp(App)
 	.use(router)
+	.component("avatar", Avatar)
 	.mount('#app')
 
