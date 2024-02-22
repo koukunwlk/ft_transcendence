2ba2809c96a0875cdbef32fbc640bd2db20d27eb
gustavdlima <gustavdlima@gmail.com>
2024-02-22 02:24:26 -0300

    feat: add websocket messages resource and enable cors

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { MessagesModule } from './messages/messages.module';
 
 @Module({
   imports: [
@@ -17,6 +18,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
         synchronize: true,
       }),
     }),
+    MessagesModule,
   ],
   controllers: [],
   providers: [],
