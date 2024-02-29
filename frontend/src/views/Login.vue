19026b89bf613dd638ce4631eda2b065ed0035f4
willian <willianportilho01@gmail.com>
2024-02-28 23:09:04 -0300

    feat: user info is shown on the login page

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,26 +1,30 @@
-<script>
+<script setup>
 import data from '../data/data.json'
-import { useCounterStore } from '../stores/counter.ts'
+import { useAuthStore } from '../stores/authStore'
 
-export default {
-	name: "Login",
-	data(){
-		return {
-			data: data,
-			counter: useCounterStore()
-		}
+const name = "Login";
+const user = useAuthStore();
+
+const login = async () => {
+	try {
+		user.setUser(data);
 	}
-};
+	catch (error) {
+		console.log(error);
+	}
+}
+
 </script>
 
 <template>
 	<div class="h-full flex justify-center items-center">
 		<div class="aspect-square h-72 w-72 flex flex-col justify-center items-center ">
 			<div>
-				<h1 class="text-3xl md:text-4xl xl:text-5xl text-white mb-3">{{ counter.count }} project</h1>
+				<h1 class="text-3xl md:text-4xl xl:text-5xl text-white mb-3">project</h1>
+				<h3 class="text-3xl md:text-4xl xl:text-5xl text-white mb-3">{{ user.getUser }}</h3>
 			</div>
 			<div>
-				<button @click="counter.increment" type="button" class="py-2.5 px-5 me-2 mb-2 text-lg md:text-xl xl:text-2xl font-bold text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Login with 42intra</button>
+				<button @click="login" class="py-2.5 px-5 me-2 mb-2 text-lg md:text-xl xl:text-2xl font-bold text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Login with 42intra</button>
 			</div>
 		</div>
 	</div>
