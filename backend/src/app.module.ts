c7b195cbf6f0565214c8c424c7b946240e440a44
Atila Costa Almeida <chaves_87@hotmail.com>
2024-03-11 03:14:46 -0300

    feat: cleaning

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
-import { AuthModule } from './auth/auth.module';
 import { LobbyModule } from './lobby/lobby.module';
 import { AppService } from './app.service';
 import { AppController } from './app.controller';
 
 @Module({
-  imports: [UserModule, AuthModule, LobbyModule],
+  imports: [UserModule, LobbyModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- backend/src/auth/42-auth/42.guards.ts
+++
@@ -1,9 +0,0 @@
-import { ExecutionContext, Injectable } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
-
-@Injectable()
-export class FortyTwoAuthGuard extends AuthGuard('42') {
-  canActivate(context: ExecutionContext) {
-    return super.canActivate(context);
-  }
-}
