ab0ab5bc72d4d29f3fb90da66ff42cd32670c748
willian <willianportilho01@gmail.com>
2024-03-18 12:58:06 -0300

    feat: add nickname (mocked). style: re-design elements position and responsive design

--- frontend/src/views/Profile.vue
+++ frontend/src/views/Profile.vue
@@ -30,31 +30,31 @@ const activateTab = (index) => {
 			Home
 		</button>
 	</div>
-	<div class="flex h-full md:h-6/6 justify-center items-center">
+	<div class="flex h-full md:h-6/6 justify-center">
 		<div class="h-full w-full md:w-3/5 bg-opacity-30 rounded-lg border border-zinc-600 text-white">
-			<div class="flex flex-col justify-center items-start h-1/3">
-				<div class="relative flex justify-center items-center ml-3 md:ml-4 lg:ml-5 xl:ml-6 2xl:ml-7">
-					<img
-						v-if="picture.getPicture"
-						:src="picture.getPicture"
-						alt="profile picture"
-						class="rounded-full object-cover w-32 h-32 md:w-36 md:h-36 lg:w-40 lg:h-40 xl:w-44 xl:h-44 2xl:w-56 2xl:h-56"
-					>
-					<!-- <Avatar
-						v-else-if="user.getUser.nickname"
-						:name=user.getUser.nickname
-						:size=80
-						borderColor="black">
-					</Avatar> -->
-					<img
-						v-else
-						src="../assets/profile-pictures/default-5.png"
-						alt="default picture"
-						class="rounded-full w-32 h-32 md:w-36 md:h-36 lg:w-40 lg:h-40 xl:w-44 xl:h-44 2xl:w-56 2xl:h-56"
-					>
-					<PicUpload></PicUpload>
+			<div class="flex flex-row h-1/3">
+				<div class="flex items-center ml-3 md:ml-4 lg:ml-5 xl:ml-6 2xl:ml-7">
+					<div class="relative w-32 h-32 md:w-36 md:h-36 lg:w-40 lg:h-40 xl:w-44 xl:h-44 2xl:w-56 2xl:h-56">
+						<img
+							v-if="picture.getPicture"
+							:src="picture.getPicture"
+							alt="profile picture"
+							class="rounded-full object-cover w-32 h-32 md:w-36 md:h-36 lg:w-40 lg:h-40 xl:w-44 xl:h-44 2xl:w-56 2xl:h-56"
+						>
+						<img
+							v-else
+							src="../assets/profile-pictures/default-5.png"
+							alt="default picture"
+							class="rounded-full w-32 h-32 md:w-36 md:h-36 lg:w-40 lg:h-40 xl:w-44 xl:h-44 2xl:w-56 2xl:h-56"
+						>
+						<PicUpload></PicUpload>
+					</div>
 				</div>
-				<!-- <p v-if="user.getUser">{{ user.getUser.nickname }}</p> teste -->
+				<div class="mt-14 ml-4">
+					<h2 v-if="user.getUser && user.getUser.nickname">{{ user.getUser.nickname }}</h2>
+					<h2 v-else>athosfilad</h2>
+					<h4 class="flex justify-end text-xs italic text-slate-300">nickname</h4>
+				</div>
 			</div>
 			<div class="flex justify-content items-center text-white text-xs md:text-base lg:text-lg xl:text-xl 2xl:text-2xl font-light h-8">
 				<button
