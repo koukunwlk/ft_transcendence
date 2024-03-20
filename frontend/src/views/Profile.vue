f4a5d1b5871bc5ac417f4f82c10fa0af71d83e81
willian <willianportilho01@gmail.com>
2024-03-19 21:29:51 -0300

    style: change profile picture size

--- frontend/src/views/Profile.vue
+++ frontend/src/views/Profile.vue
@@ -52,18 +52,18 @@ const openSettings = () => {
 			</button>
 			<div class="flex flex-row h-1/3">
 				<div class="flex items-center ml-3 md:ml-4 lg:ml-5 xl:ml-6 2xl:ml-7">
-					<div class="relative w-32 h-32 md:w-36 md:h-36 lg:w-40 lg:h-40 xl:w-44 xl:h-44 2xl:w-56 2xl:h-56">
+					<div class="relative w-32 h-32 md:w-36 md:h-36 xl:w-40 xl:h-40 2xl:w-52 2xl:h-52">
 						<img
 							v-if="picture.getPicture"
 							:src="picture.getPicture"
 							alt="profile picture"
-							class="rounded-full object-cover w-32 h-32 md:w-36 md:h-36 lg:w-40 lg:h-40 xl:w-44 xl:h-44 2xl:w-56 2xl:h-56"
+							class="rounded-full object-cover"
 						>
 						<img
 							v-else
 							src="../assets/profile-pictures/default-5.png"
 							alt="default picture"
-							class="rounded-full w-32 h-32 md:w-36 md:h-36 lg:w-40 lg:h-40 xl:w-44 xl:h-44 2xl:w-56 2xl:h-56"
+							class="rounded-full object-cover"
 						>
 						<PicUpload></PicUpload>
 					</div>
