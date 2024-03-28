4d79f96adf800d472286cc2a0c2a2ad3a5dd1e33
willian <willianportilho01@gmail.com>
2024-03-28 19:22:20 -0300

    feat: design responsive (settings)

--- frontend/src/components/Settings.vue
+++ frontend/src/components/Settings.vue
@@ -10,11 +10,16 @@ export default {
 
 <template>
     <div class="fixed top-0 left-0 w-screen h-screen flex flex-col justify-center items-center bg-black bg-blur bg-opacity-80 z-20">
-        <div class="w-80 h-96 bg-gray-900 rounded-xl">
+        <div class="w-64 h-80 md:w-80 md:h-96 lg:w-1/4 lg:h-2/4  bg-gray-900 rounded-xl">
             <div class="flex justify-end mr-2 mt-2">
                 <button @click="emitEvent">
                     <img class="h-6 w-6" src="../assets/svgs/x-icon.svg" alt="close modal icon">
                 </button>
+            </div>
+            <div class="flex justify-center">
+                <h1 class="text-3xl lg:text-4xl">
+                    Settings
+                </h1>
             </div>
         </div>
     </div>
