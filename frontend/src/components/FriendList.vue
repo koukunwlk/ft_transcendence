5885a335faaddc6ab9f4de9d587d26d4cb8f19fb
gustavdlima <gustavdlima@gmail.com>
2024-03-27 22:57:41 -0300

    feat: button with cvg icon to render add friend modal added

--- frontend/src/components/FriendList.vue
+++ frontend/src/components/FriendList.vue
@@ -1,22 +1,32 @@
 <template>
     <div
     class="flex flex-col bg-gray-200 items-center h-full w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 overflow-scroll">
-        <div class="bg-red-200">
-            Navbar
+        <div class="bg-black">
+            <div class="bg-red-300 ">
+                <button class="px-4 py-2 bg-blue-200 text-white rounded"  @click="openAddFriendModal">
+                    <svg class="w-6 h-6 text-gray-800 dark:text-black " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 12h4m-2 2v-4M4 18v-1a3 3 0 0 1 3-3h4a3 3 0 0 1 3 3v1a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1Zm8-10a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"/>
+                    </svg>
+                </button>
+                <OneInputModal :isOpen="isAddFriendModalOpen" @close-modal="closeAddFriendModal" :title="'Add Friend'" />
+            </div>
         </div>
         <div v-for="friend in friends" :key="friend.id" class="w-full p-2">
             <FriendComponent :friend="friend" />
         </div>
     </div>
+
 </template>
 
 <script>
 import FriendComponent from './FriendComponent.vue';
+import OneInputModal from './OneInputModal.vue';
 
 export default {
     name: 'FriendList',
     components: {
         FriendComponent,
+        OneInputModal,
     },
     data() {
         return {
@@ -34,7 +44,16 @@ export default {
                 { id: 11, name: 'Isaac Newton', status: 1 },
                 { id: 12, name: 'Jack Sparrow', status: 2 },
             ],
+            isAddFriendModalOpen: false,
         };
+    },
+    methods: {
+        openAddFriendModal() {
+            this.isAddFriendModalOpen = true;
+        },
+        closeAddFriendModal() {
+            this.isAddFriendModalOpen = false;
+        }
     },
 };
 </script>
