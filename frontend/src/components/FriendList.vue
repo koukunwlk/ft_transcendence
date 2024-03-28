2f49880635290eca90faeefd9572e95a9e020516
gustavdlima <gustavdlima@gmail.com>
2024-03-28 11:20:39 -0300

    refactor: remove modal from community button

--- frontend/src/components/FriendList.vue
+++ frontend/src/components/FriendList.vue
@@ -32,17 +32,14 @@
 
             <!-- Community Button -->
             <div class="bg-red-300 ">
-                <button class="px-4 py-2 bg-blue-200 text-white rounded" @click="openBlockFriendModal">
+                <button class="px-4 py-2 bg-blue-200 text-white rounded">
                     <svg class="w-6 h-6 text-gray-800 dark:text-black" aria-hidden="true"
                         xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                         <path stroke="currentColor" stroke-linecap="round" stroke-width="2"
                             d="M4.5 17H4a1 1 0 0 1-1-1 3 3 0 0 1 3-3h1m0-3.05A2.5 2.5 0 1 1 9 5.5M19.5 17h.5a1 1 0 0 0 1-1 3 3 0 0 0-3-3h-1m0-3.05a2.5 2.5 0 1 0-2-4.45m.5 13.5h-7a1 1 0 0 1-1-1 3 3 0 0 1 3-3h3a3 3 0 0 1 3 3 1 1 0 0 1-1 1Zm-1-9.5a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0Z" />
                     </svg>
 
-
                 </button>
-                <OneInputModal :isOpen="isBlockFriendModalOpen" @close-modal="closeBlockFriendModal"
-                    :title="'User to block:'" />
             </div>
 
             <!-- Notification Button -->
