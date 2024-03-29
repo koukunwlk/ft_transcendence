c1d291ac1ed685008d84a076cf89de54718b11e8
gustavdlima <gustavdlima@gmail.com>
2024-03-28 21:52:51 -0300

    feat: chat stylezed and functional with mocked values

--- frontend/src/components/Chat.vue
+++ frontend/src/components/Chat.vue
@@ -1,19 +1,31 @@
 <template>
-	<div class="h-full w-full
-		bg-blue-200 grid grid-rows-8 gap-1">
-			<div class="grid row-span-1 bg-green-200">
-				<div class="h-full w-full
-		bg-blue-200 grid grid-rows-8 gap-1">
-
-				</div>
-			</div>
-			<div class="row-span-6 bg-red-200">
-				b
-			</div>
-			<div class="row-span-1 bg-purple-200">
-				c
-			</div>
-	</div>
+    <div v-if="friend" class="h-full w-full grid grid-rows-8 gap-1 ">
+        <div class="grid row-span-1 ">
+            <div class="h-full w-full grid grid-cols-6 shadow-xl shadow-yellow-500/5 ">
+                <div class="col-span-1 grid justify-center"> <img class="img-fluid rounded-full w-16 h-16 mt-1"
+                        src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                        alt="Sunset in the mountains"> </div>
+                <div class="col-span-5 grid justify-start content-center"> <span class="text-3xl font-bold ml-5">
+                    {{ friend.name }} </span> </div>
+            </div>
+        </div>
+        <div class="chat-box row-span-6 max-h-96 overflow-y-auto">
+            <div class="max-h-25 m-5 grid gap-1 ">
+                <div v-for="message in messages"
+                    class="bg-gray-700 m-2 rounded-lg h-10 text-xl grid content-center p-2 "> {{ message }} </div>
+            </div>
+        </div>
+        <div class="row-span-1 ">
+            <div class="h-full w-full grid grid-cols-6 gap-1">
+                <div class="col-span-5 grid content-center p-3"> <input type="text"
+                        class="min-h-8 min-w-full rounded-md border-gray-700 shadow-sm focus:border-yellow-500 focus:ring-yellow-500 p-1 text-black"
+                        placeholder="..." v-model="messageToSend" /> </div>
+                <div class="col-span-1 grid justify-center content-center"> <button
+                        class="min-h-8 bg-gray-600 border-2 min-w-16 cursor-pointer rounded-md border-gray-700 shadow-sm shadow-yellow-500/5 focus:border-yellow-500 focus:ring-yellow-500 p-1 text-white grid justify-center"
+                        @click="sendMessage"> Send </button> </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -21,37 +33,26 @@ export default {
     name: 'FriendList',
     components: {
     },
+    props: {
+        friend: {
+            type: Object
+        }
+    },
     data() {
         return {
-            friends: [
-                { id: 1, name: 'John Doe', status: 0 },
-                { id: 2, name: 'Jane Doe', status: 1 },
-                { id: 3, name: 'Bob Smith', status: 2 },
-                { id: 4, name: 'Alice Johnson', status: 3 },
-                { id: 5, name: 'Charlie Brown', status: 4 },
-                { id: 6, name: 'Diana Prince', status: 0 },
-                { id: 7, name: 'Edward Norton', status: 1 },
-                { id: 8, name: 'Fiona Apple', status: 0 },
-                { id: 9, name: 'George Washington', status: 1 },
-                { id: 10, name: 'Henry Ford', status: 0 },
-                { id: 11, name: 'Isaac Newton', status: 1 },
-                { id: 12, name: 'Jack Sparrow', status: 2 },
-                { id: 13, name: 'Kevin Mitnick', status: 3 },
-                { id: 14, name: 'Lucas Arnus', status: 4 },
+            messages: [
+                "Oi amigo",
+                "Vamos jogar aquele pong?",
+                "Manda link ae"
             ],
-            friendRequests: [
-                { id: 1, name: 'John Doe' },
-                { id: 2, name: 'Jane Doe' },
-                { id: 3, name: 'Bob Smith' },
-                { id: 4, name: 'Alice Johnson' },
-                { id: 5, name: 'Charlie Brown' },
-                { id: 6, name: 'Diana Prince' },
-                { id: 7, name: 'Edward Norton' },
-                { id: 8, name: 'Fiona Apple' },
-            ],
+            messageToSend: null,
         };
     },
     methods: {
+        sendMessage() {
+            this.messages[this.messages.length] = this.messageToSend;
+            this.messageToSend = null;
+        }
     },
 };
 </script>
