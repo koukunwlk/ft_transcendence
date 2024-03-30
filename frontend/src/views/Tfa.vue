19144974c76bfdbba2e08da5c998b3c681088609
willian <willianportilho01@gmail.com>
2024-03-30 19:52:13 -0300

    feat: complete design response; add: pong logo

--- frontend/src/views/Tfa.vue
+++ frontend/src/views/Tfa.vue
@@ -3,36 +3,40 @@
     v-if="Object.hasOwn(user, 'username')"
     class="justify-center flex items-center h-screen"
   >
-    <div class="h-96 w-72 md:w-80 text-gray-300 rounded-lg border border-gray-500 flex flex-col justify-center items-center">
-      <div class="text-2xl tracking-[-.08rem] md:text-2xl mb-5">
+    <div class="h-[412px] w-72 md:h-[432px] xl:h-[452px] md:w-96 xl:w-[412px] text-gray-300 rounded-lg border border-gray-500 flex flex-col justify-center items-center">
+      <img
+        class="h-12 md:h-16 xl:h-20 mt-4 xl:mt-0 mb-5"
+        src="../assets/logo/logo.png" alt="logo image"
+      >
+      <div class="text-2xl tracking-[-.08rem] lg:text-3xl mb-5">
         <p>Two-factor authentication</p>
       </div>
-      <div class="flex flex-col items-center justify-center h-40 w-64 rounded-sm border border-gray-700">
+      <div class="flex flex-col items-center justify-center h-40 lg:h-44 w-64 md:w-80 xl:w-[352px] rounded-sm border border-gray-700">
         <div class="flex flex-col">
-          <div class="flex text-sm font-semibold mb-2">
+          <div class="flex text-sm lg:text-base md:font-semibold lg:font-normal xl:font-semibold mb-2">
             Authentication code:
           </div>
           <div class="flex justify-center text-4xl">
             <input
               type="text"
-              class="text-start w-56 md:w-64 lg:w-80 tracking-[.25em] md:tracking-[.35em] lg:tracking-[.45em] xl:tracking-[.35em] font-semibold bg-gray-100 p-2.5 border border-gray-300 text-gray-900 text-sm lg:text-base xl:text-lg rounded-sm focus:border-blue-500 block dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:focus:ring-blue-500"
+              class="text-start w-56 md:w-72 xl:w-80 tracking-[.25em] md:tracking-[.35em] lg:tracking-[.45em] xl:tracking-[.35em] font-semibold bg-gray-100 p-2.5 border border-gray-300 text-gray-900 text-sm lg:text-base xl:text-lg rounded-sm focus:border-blue-500 block dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:focus:ring-blue-500"
               placeholder="Ex: ABCDEFGHIJ..."
             >
           </div>
         </div>
         <button
           @click="validateCode"
-          class="mt-4 w-56 md:w-64 lg:w-80 hover:bg-green-500 bg-green-600 text-sm font-semibold text-gray-100 hover:text-white p-2.5 border border-green-700 hover:border-green-500 rounded-sm block placeholder-gray-400 focus:ring-blue-500">
+          class="mt-4 w-56 md:w-72 xl:w-80 hover:bg-green-500 bg-green-600 text-sm font-semibold text-gray-100 hover:text-white p-2.5 border border-green-700 hover:border-green-500 rounded-sm block placeholder-gray-400 focus:ring-blue-500">
           Verify
         </button>
       </div>
-      <div class="flex items-start mt-6">
+      <div class="flex items-start mt-6 md:mt-8">
         <img
-          class="h-5 w-5 ml-3 mr-2"
+          class="h-5 w-5 ml-3 md:ml-7 mr-2"
           src="../assets/svgs/phone.svg"
           alt="phone icon"
         >
-        <span class="text-xs mr-3">
+        <span class="text-xs md:text-sm mr-3 md:mr-7">
           Hi, {{ user.username }}! Open the two-factor authentication app on your device to view your authentication code and verify your identity.
         </span>
       </div>
