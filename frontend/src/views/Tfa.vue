7a78b36a7e2bd35aeac7a821b2336b7d34276cc3
willian <willianportilho01@gmail.com>
2024-03-30 19:55:34 -0300

    fix: placeholder example

--- frontend/src/views/Tfa.vue
+++ frontend/src/views/Tfa.vue
@@ -19,8 +19,8 @@
           <div class="flex justify-center text-4xl">
             <input
               type="text"
-              class="text-start w-56 md:w-72 xl:w-80 tracking-[.25em] md:tracking-[.35em] lg:tracking-[.45em] xl:tracking-[.35em] font-semibold bg-gray-100 p-2.5 border border-gray-300 text-gray-900 text-sm lg:text-base xl:text-lg rounded-sm focus:border-blue-500 block dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:focus:ring-blue-500"
-              placeholder="Ex: ABCDEFGHIJ..."
+              class="text-start w-56 md:w-72 xl:w-80 tracking-[.35em] md:tracking-[.45em] lg:tracking-[.55em] xl:tracking-[.45em] font-semibold bg-gray-100 p-2.5 border border-gray-300 text-gray-900 text-sm lg:text-base xl:text-lg rounded-sm focus:border-blue-500 block dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:focus:ring-blue-500"
+              placeholder="Ex: 123456"
             >
           </div>
         </div>
