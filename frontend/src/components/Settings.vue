8c27df5010ccb13c45e399056d3cbfa4e680cb58
willian <willianportilho01@gmail.com>
2024-03-30 00:59:37 -0300

    style: change border style

--- frontend/src/components/Settings.vue
+++ frontend/src/components/Settings.vue
@@ -75,12 +75,12 @@ export default {
                             v-model="nickname"
                             type="text"
                             id="nickname"
-                            class="bg-gray-50 w-4/6 p-2.5 border border-gray-300 text-gray-900 text-sm rounded-sm focus:border-blue-500 block dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500"
+                            class="bg-gray-500 w-4/6 p-2.5 border border-gray-300 text-gray-900 text-sm rounded-l-sm focus:border-blue-500 block dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500"
                         />
                         <button
                             type="button"
                             @click="saveNickname"
-                            class="bg-green-500 hover:bg-green-400 text-sm text-gray-100 p-2.5 border border-green-400 rounded-sm block placeholder-gray-400 focus:ring-blue-500 -ml-1"
+                            class="bg-green-500 hover:bg-green-400 text-sm text-gray-100 p-2.5 border border-green-400 rounded-r-sm block placeholder-gray-400 focus:ring-blue-500"
                         >
                             save
                         </button>
