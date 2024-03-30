3eebb8e906eaef9982c2488eb13b70b3e713cc94
willian <willianportilho01@gmail.com>
2024-03-29 22:45:29 -0300

    style: nickname semibold

--- frontend/src/components/Settings.vue
+++ frontend/src/components/Settings.vue
@@ -46,22 +46,46 @@ export default {
                 </h1>
             </div>
             <div class="h-4/6 w-full flex flex-col justify-center ml-4 md:ml-6 xl:ml-8 2xl:ml-12">
-                <div for="2f" class="flex flex-row justify-start mb-5">
+                <div class="mb-5">
+                    <label
+                        for="nickname"
+                        class="block text-sm lg:text-lg lg:font-semibold text-gray-900 dark:text-gray-300"
+                    >
+                        Nickname
+                    </label>
+                    <div class="flex items-center">
+                        <input
+                            placeholder="enter new nickname" 
+                            required
+                            v-model="nickname"
+                            type="text"
+                            id="nickname"
+                            class="bg-gray-50 w-4/6 p-2.5 border border-gray-300 text-gray-900 text-sm rounded-sm focus:border-blue-500 block dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500"
+                        />
+                        <button
+                            type="button"
+                            @click="saveNickname"
+                            class="bg-green-500 text-sm text-gray-900 dark:text-gray-300 p-2.5 border-green-300 rounded-sm block dark:bg-green-500 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 ml-1"
+                        >
+                            save
+                        </button>
+                    </div>
+                </div>
+                <div for="2f" class="flex flex-row justify-start">
                     <label class="inline-flex items-center cursor-pointer">
-                        <input v-model="isChecked" @change="handleToggleChange" type="checkbox" id="2f" class="sr-only peer">
+                        <input
+                            v-model="isChecked"
+                            @change="handleToggleChange"
+                            type="checkbox"
+                            id="2f"
+                            class="sr-only peer"
+                        >
                         <div class="relative w-11 h-6 peer-focus:outline-none rounded-full peer bg-gray-500 dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-green-500">
                         </div>
                     </label>
                     <span class="ms-3 text-base md:text-lg lg:text-xl 2xl:text-2xl text-gray-900 dark:text-gray-300">
                         2F Authentication
                     </span>
-                </div>
-                <div class="pt-3">
-                    <label for="nickname" class="block text-sm lg:text-lg text-gray-900 dark:text-gray-300">Nickname</label>
-                    <div class="flex items-center">
-                        <input placeholder="enter new nickname" required v-model="nickname" type="text" id="nickname" class="bg-gray-50 w-4/6 p-2.5 border border-gray-300 text-gray-900 text-sm rounded-sm focus:border-blue-500 block dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500"/>
-                        <button type="button" @click="saveNickname" class="bg-green-500 text-sm text-gray-900 dark:text-gray-300 p-2.5 border-green-300 rounded-sm block dark:bg-green-500 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 ml-1">save</button>
-                    </div>
                 </div>
             </div>
         </div>
