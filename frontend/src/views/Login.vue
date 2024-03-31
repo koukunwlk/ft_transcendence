c08470589c2c2c1335803300cf6026f8c3520ad5
willian <willianportilho01@gmail.com>
2024-03-31 01:17:26 -0300

    feat: add pong logo; style: add design response and styles.

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,37 +1,3 @@
-<template>
-  <div class="bg-login-background bg-cover h-full flex justify-center items-center">
-    <div
-      class="aspect-square h-72 w-72 flex flex-col justify-center items-center"
-    >
-      <div>
-        <h1 class="text-3xl md:text-4xl xl:text-5xl text-white mb-3">
-          42 project
-        </h1>
-      </div>
-      <div>
-        <button
-          @click="login"
-          class="py-2.5 px-5 me-2 mb-2 text-lg md:text-xl xl:text-2xl font-bold text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
-        >
-          Login with 42intra
-        </button>
-        <button
-          @click="goHome"
-          class="py-2.5 px-5 me-2 mb-2 text-lg md:text-xl xl:text-2xl font-bold text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
-        >
-          Home
-        </button>
-      </div>
-      <!-- <h3
-        v-if="user.getUser"
-        class="text-3xl md:text-2xl xl:text-1xl text-white mb-3"
-      >
-        Welcome {{ user.getUser.nickname }}
-      </h3> -->
-    </div>
-  </div>
-</template>
-
 <script>
 import data from "../data/data.json";
 import userService from "../services/UserService";
@@ -87,3 +53,49 @@ export default {
   },
 };
 </script>
+
+<template>
+	<div class="w-screen h-screen overflow-hidden margin-0">
+		<div class="flex image-animation">
+			<img class="flex h-screen object-cover justify-center items-center" src="../assets/images/kai-pilger.jpg" alt="background">
+			<img class="flex h-screen object-cover justify-center items-center" src="../assets/images/kai-pilger.jpg" alt="background">
+		</div>
+		<div class="absolute inset-0 flex flex-col justify-center items-center h-4/6">
+      <div>
+        <img
+          class="w-48 md:w-56 lg:w-72 xl:w-80 2xl:w-96 mb-5"
+          src="../assets/logo/logo.png"
+          alt="logo image"
+        >
+      </div>
+      <div>
+        <button
+          @click="login"
+          type="button"
+          class="border font-mono border-gray-600 font-medium text-sm md:text-base xl:text-lg px-5 md:px-7 xl:px-8 py-2.5 md:py-3.5 xl:py-4 mb-2 text-white bg-[#24292F]/80 hover:bg-[#24292F]/90 focus:ring-4 focus:outline-none focus:ring-[#24292F]/50 rounded-lg text-center inline-flex items-center dark:focus:ring-gray-500 dark:hover:bg-[#050708]/30">
+            <span class="italic font-serif mr-2">
+              42
+            </span>
+          Sign in with Intra
+        </button>
+      </div>
+    </div>
+	</div>
+</template>
+
+<style>
+
+.image-animation {
+	animation: moveImages 300s infinite linear;
+}
+
+@keyframes moveImages {
+	0% {
+		transform: translate(0);
+	}
+	100% {
+		transform: translate(-50%);
+	}
+}
+
+</style>
