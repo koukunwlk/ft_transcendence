698d31a7708a56c16aa0b7f2b67ca7e4d3d5c950
Atila Costa Almeida <chaves_87@hotmail.com>
2024-03-31 17:20:05 -0300

    FriendService.ts update

---
+++ frontend/src/services/FriendService.ts
@@ -0,0 +1,63 @@
+import { useAuthStore } from "../stores/authStore";
+import axios, { AxiosInstance } from "axios";
+
+export enum FriendListStatusEnum {
+    PENDING = "PENDING",
+    ACCEPTED = "ACCEPTED",
+    REJECTED = "REJECTED",
+    BLOCKED = "BLOCKED",
+    CANCELLED = "CANCELLED",
+}
+
+
+export class FriendService {
+	private authStore = useAuthStore();
+	private token: string;
+	private friendListClient: AxiosInstance
+	constructor() {
+		const cookies = document.cookie.split(";");
+		cookies.forEach((cookie) => {
+			if (cookie.startsWith("token=")) {
+				this.saveToken(cookie);
+			}
+		});
+		this.token = this.authStore.token;
+		this.friendListClient = axios.create({
+			baseURL: "http://10.0.0.173:3000/friend-controller/",
+			headers: {
+				Authorization: `Bearer ${this.token}`,
+				token: this.token,
+			},
+		});
+	}
+
+	getReceivedFriendRequests() {
+		return this.friendListClient.get("received-friend-requests/");
+	}
+
+	getSendedFriendRequests() {
+		return this.friendListClient.get("sended-friend-requests/");
+	}
+
+	getFriendList() {
+		return this.friendListClient.get("friend-list/");
+	}
+
+	sendFriendRequest(receiverNickname: string) {
+		return this.friendListClient.post("send-friend-request", {
+			receiverNickname,
+		});
+	}
+
+	handleFriendRequest(friendId: string, status: string) {
+		return this.friendListClient.post("handle-friend-request/", {
+			friendId,
+			status,
+		});
+	}
+
+	private saveToken(tokenCookie: string) {
+		const token = tokenCookie.substring(6);
+		this.authStore.setToken(token);
+	}
+}
