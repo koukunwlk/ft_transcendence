6faac393890ef911bb41854871344e4c275fc2f3
gustavdlima <gustavdlima@gmail.com>
2024-03-31 11:12:49 -0300

    feat: add stylezed logout button

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -3,41 +3,55 @@
     <Settings @clickedButton="closeModal" />
   </div>
   <div
-    class="grid grid-rows-2 sm:grid-cols-4 lg:grid-cols-6 xl:grid-cols-8 h-full justify-center items-center border-4 overflow-hidden">
-    <div
-      class="row-span-1 sm:col-span-3 lg:col-span-4 xl:col-span-6 justify-center items-center h-full w-full mt-36 sm:mt-36 xl:mt-60">
-      <div class="grid grid-rows-2 sm:grid-cols-2 justify-center content-center">
-        <div class="row-span-1 sm:col-span-1 grid justify-center items-center sm:mt-36 mb-2 sm:mb-0">
-          <button type="button" @click="lobbyRedirect"
-            class="h-32 w-32 sm:h-56 sm:w-56 lg:h-64 lg:w-64 xl:ml-48 bg-gray-600 bg-opacity-30 hover:bg-zinc-500 text-zinc-500  font-semibold hover:text-white text-sm focus:outline-none rounded-lg border border-zinc-600 hover:border-transparent focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700">
-            <div class="flex justify-center items-center">
-              <svg class="h-16 w-16 sm:h-28 sm:w-28" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24"
-                height="24" fill="currentColor" viewBox="0 0 24 24">
-                <path fill-rule="evenodd"
-                  d="M10.271 5.575C8.967 4.501 7 5.43 7 7.12v9.762c0 1.69 1.967 2.618 3.271 1.544l5.927-4.881a2 2 0 0 0 0-3.088l-5.927-4.88Z"
-                  clip-rule="evenodd" />
-              </svg>
-            </div>
-          </button>
+    class="grid grid-rows-2 sm:grid-cols-4 lg:grid-cols-8 h-full w-full justify-center items-center border-4 overflow-hidden">
+    <div class="row-span-1 sm:col-span-3 lg:col-span-6 justify-center items-center mt-28 lg:mt-12 xl:mt-0">
+      <div class="grid sm:grid-rows-3 justify-center content-center">
+        <!-- Play and Profile Buttons -->
+        <div class="sm:row-span-2 grid sm:content-end">
+          <div class="grid grid-cols-2 gap-6">
+            <button type="button" @click="lobbyRedirect"
+              class="h-24 w-24 sm:h-56 sm:w-56 lg:h-60 lg:w-60 xl:h-80 xl:w-80 bg-gray-600 bg-opacity-30 hover:bg-zinc-500 text-zinc-500  font-semibold hover:text-white text-sm focus:outline-none rounded-lg border border-zinc-600 hover:border-transparent focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700">
+              <div class="flex justify-center items-center">
+                <svg class="h-16 w-16 sm:h-28 sm:w-28" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24"
+                  height="24" fill="none" viewBox="0 0 24 24">
+                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                    d="m9 5 7 7-7 7" />
+                </svg>
+
+              </div>
+            </button>
+            <button type="button" @click="profileRedirect()"
+              class="h-24 w-24 sm:h-56 sm:w-56 lg:h-60 lg:w-60 xl:h-80 xl:w-80 bg-gray-600 bg-opacity-30 hover:bg-zinc-500 text-zinc-500  font-semibold hover:text-white text-sm focus:outline-none rounded-lg border border-zinc-600 hover:border-transparent focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700">
+              <div class="grid justify-center">
+                <svg class="h-16 w-16 sm:h-28 sm:w-28" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24"
+                  height="24" fill="currentColor" viewBox="0 0 24 24">
+                  <path fill-rule="evenodd"
+                    d="M12 4a4 4 0 1 0 0 8 4 4 0 0 0 0-8Zm-2 9a4 4 0 0 0-4 4v1a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2v-1a4 4 0 0 0-4-4h-4Z"
+                    clip-rule="evenodd" />
+                </svg>
+              </div>
+            </button>
+          </div>
         </div>
-        <div class="row-span-1 sm:col-span-1 grid justify-center items-center sm:mt-36 ">
-          <button type="button" @click="profileRedirect()"
-            class="h-32 w-32 sm:h-56 sm:w-56 lg:h-64 lg:w-64 xl:mr-48 bg-gray-600 bg-opacity-30 hover:bg-zinc-500 text-zinc-500  font-semibold hover:text-white text-sm focus:outline-none rounded-lg border border-zinc-600 hover:border-transparent focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700">
-            <div class="grid justify-center">
-              <svg class="h-16 w-16 sm:h-28 sm:w-28" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24"
-                height="24" fill="currentColor" viewBox="0 0 24 24">
-                <path fill-rule="evenodd"
-                  d="M12 4a4 4 0 1 0 0 8 4 4 0 0 0 0-8Zm-2 9a4 4 0 0 0-4 4v1a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2v-1a4 4 0 0 0-4-4h-4Z"
-                  clip-rule="evenodd" />
-              </svg>
-            </div>
-          </button>
-          <button @click="logout">Logout</button>
+        <!-- Logout buttom -->
+        <div class="sm:row-span-1 mt-3 xl:mt-0 xl:ml-44">
+          <div class=" grid justify-center content-start ">
+            <button type="button" @click="logout()"
+              class="h-24 w-24 sm:h-56 sm:w-56 lg:h-60 lg:w-60 xl:mt-8 xl:h-80 xl:w-80 xl:mr-48 bg-gray-600 bg-opacity-30 hover:bg-zinc-500 text-zinc-500  font-semibold hover:text-white text-sm focus:outline-none rounded-lg border border-zinc-600 hover:border-transparent focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700">
+              <div class="grid justify-center">
+                <svg class="h-16 w-16 sm:h-28 sm:w-28" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24"
+                  height="24" fill="none" viewBox="0 0 24 24">
+                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                    d="M16 12H4m12 0-4 4m4-4-4-4m3-4h2a3 3 0 0 1 3 3v10a3 3 0 0 1-3 3h-2" />
+                </svg>
+
+              </div>
+            </button>
+          </div>
         </div>
       </div>
     </div>
-    <div
-      class="row-span-2 sm:col-span-1 lg:col-span-2 xl:col-span-2 overflow-x-hidden max-h-full max-w-full mt-16 sm:mt-0">
+    <div class="row-span-2 sm:col-span-1 lg:col-span-2 overflow-x-hidden max-h-full max-w-full mt-36 sm:mt-0">
       <FriendList @actual-friend="getFriendOnClick" />
     </div>
   </div>
